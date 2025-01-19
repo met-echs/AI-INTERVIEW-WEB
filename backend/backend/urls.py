d51a27924a3819c8e6dc8bffae57bfd007193e70
Amil <amilmether37@gmail.com>
2025-01-19 19:43:18 +0530

    Login Page Added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apply/', include('ApplyPage.urls')),
+    path('interview/', include('Interview.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
